25c0c91c8fef95c4200fb4579824e1115c8d443d
Ronald Cheng <ronaldcheng14@gmail.com>
2016-03-02 18:07:13 -0800

    mysqldb connect created

---
+++ server-side/required.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+# http://www.tutorialspoint.com/python/python_database_access.htm
+# http://mysql-python.sourceforge.net/mdb.html
+# http://zetcode.com/db/mysqlpython/
+# http://www.mysqltutorial.org/python-connecting-mysql-databases/
+
+import MySQLdb as mdb 		# _mysql Python wrapper
+import sys
+import json
+
+class Connection:
+
+	__database		= None
+	__instance 		= None 	
+	__connection 	= None
+	__session 		= None 		# Database cursor object
+
+	def __new__(cls, *args, **kwargs):
+		if not cls.__instance and not cls.__database:
+			cls.__instance = super(Connection, cls).__new__(cls, *args, **kwargs)
+		else:
+			return cls.__instance
+	## End def __new__
+
+	def __init__(self):
+		try:
+			with open('security/config.json', 'r') as f:
+				cfg = json.load(f)["mysql"]		# Access credentials
+			self.__connect(cfg['host'], cfg['user'], cfg['passwd'], cfg['db'])
+			f.close()
+		except IOError as e:
+			print e
+	## End def __init__
+
+	def __connect(self, host='', user='', passwd='', database=''):
+		self.__database = database
+		try:
+			conn = mdb.connect(host, user, passwd, self.__database)
+			self.__connection = conn
+			self.__session = conn.cursor()
+
+			self.__session.execute("SELECT VERSION()");
+			ver = self.__session.fetchone()
+			print "[SERVER] Running ver: %s " % ver
+
+		except mdb.Error as e:
+			print "Error %d: %s" % (e.args[0], e.args[1])
+			sys.exit(1)
+	## End def __connect
+
+	def close(self):	
+		if self.__session:
+			self.__session.close()
+			self.__connection.close()
+	## End def close
+
+t = Connection()
