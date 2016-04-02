1a845b2bb236ae0ec5c3ddf0bb1b64b59eae7546
Ronald Cheng <ronaldcheng14@gmail.com>
2016-04-02 00:09:42 -0700

    fix Connector class use of static variables

--- server-side/Connector.py
+++ server-side/Connector.py
@@ -1,59 +1,69 @@
 #!/usr/bin/python
 
-# http://www.tutorialspoint.com/python/python_database_access.htm
-# http://mysql-python.sourceforge.net/mdb.html
-# http://zetcode.com/db/mysqlpython/
-# http://www.mysqltutorial.org/python-connecting-mysql-databases/
+# References
+# 
+# -	http://www.mikusa.com/python-mysql-docs/index.html
+# -	http://mysql-python.sourceforge.net/mdb.html
+# -	http://zetcode.com/db/mysqlpython/
+# 
 
 import MySQLdb as mdb 		# _mysql Python wrapper
 import sys
 import json
 
-class Connection:
+class Connection(object):
 
 	__database		= None
-	__instance 		= None 	
+	__instance  	= None 	
 	__connection 	= None
 	__session 		= None 		# Database cursor object
 
-	def __new__(cls, *args, **kwargs):
-		if not cls.__instance and not cls.__database:
-			cls.__instance = super(Connection, cls).__new__(cls, *args, **kwargs)
-		else:
-			return cls.__instance
-	## End def __new__
-
 	def __init__(self):
+		# Controls initialization of a new instance
 		try:
 			with open('security/config.json', 'r') as f:
 				cfg = json.load(f)["mysql"]		# Access credentials
-			self.__connect(cfg['host'], cfg['user'], cfg['passwd'], cfg['db'])
+			
+			self.__connect(cfg['host'], cfg['user'], cfg['passwd'], cfg['db'])	
 			f.close()
+		
 		except IOError as e:
 			print e
+
 	## End def __init__
 
 	def __connect(self, host='', user='', passwd='', database=''):
-		self.__database = database
-		try:
-			conn = mdb.connect(host, user, passwd, self.__database)
-			self.__connection = conn
-			# Set data retrieval return type as dictionary
-			self.__session = conn.cursor(mdb.cursors.DictCursor)
+		if Connection.__session is not None:
+			# Prevent mulitple instance of connection
+			return
 
-			self.__session.execute("SELECT VERSION()");
-			ver = self.__session.fetchone()
+		Connection.__database = database
+
+		try:
+			if not Connection.__database:
+				raise NameError('Unspecified database');
+
+			conn = mdb.connect(host, user, passwd, Connection.__database)
+			Connection.__connection = conn
+
+			# Set data retrieval return type as dictionary
+			Connection.__session = conn.cursor(mdb.cursors.DictCursor)
+
+			Connection.__session.execute("SELECT VERSION()");
+			ver = Connection.__session.fetchone()
 			print "[SERVER] Database ver: %s " % ver
 
 		except mdb.Error as e:
 			print "Error %d: %s" % (e.args[0], e.args[1])
 			sys.exit(1)
+
 	## End def __connect
 
 	def close(self):	
-		if self.__session:
-			self.__session.close()
-			self.__connection.close()
+		if Connection.__session:
+			Connection.__session.close()
+			Connection.__connection.close()
+
 	## End def close
 
 def main():
