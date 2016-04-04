18d19ec8d37fd6e39ed64c41a8bfd22e55020378
Ronald Cheng <ronaldcheng14@gmail.com>
2016-04-04 12:33:00 -0700

    changed DB connector, returns single instance using connect(), getSession()

--- server-side/Connector.py
+++ server-side/Connector.py
@@ -5,75 +5,83 @@
 # -	http://www.mikusa.com/python-mysql-docs/index.html
 # -	http://mysql-python.sourceforge.net/mdb.html
 # -	http://zetcode.com/db/mysqlpython/
-# 
+# - https://www.python.org/dev/peps/pep-0249/
+#
 
 import MySQLdb as mdb 		# _mysql Python wrapper
-import sys
-import json
+from sys import exit
+from json import load
 
-class Connection(object):
+class Database(object):
 
-	__database		= None
-	__instance  	= None 	
+	__database		= None	
 	__connection 	= None
 	__session 		= None 		# Database cursor object
 
 	def __init__(self):
 		# Controls initialization of a new instance
+		pass
+
+	@classmethod
+	def connect(cls):
 		try:
 			with open('security/config.json', 'r') as f:
-				cfg = json.load(f)["mysql"]		# Access credentials
+				cfg = load(f)["mysql"]		# Access credentials
 			
-			self.__connect(cfg['host'], cfg['user'], cfg['passwd'], cfg['db'])	
+			cls.__connect(cfg['host'], cfg['user'], cfg['passwd'], cfg['db'])	
 			f.close()
+
+			return cls.__connection 	# Return conn instance
 		
 		except IOError as e:
 			print e
 
-	## End def __init__
+	@classmethod
+	def getSession(cls):
+		return cls.__session
 
-	def __connect(self, host='', user='', passwd='', database=''):
-		if Connection.__session is not None:
+	@classmethod
+	def __connect(cls, host='', user='', passwd='', database=''):
+		if cls.__session is not None:
 			# Prevent mulitple instance of connection
-			return
+			return 
 
-		Connection.__database = database
+		cls.__database = database
 
 		try:
-			if not Connection.__database:
+			if not cls.__database:
 				raise NameError('Unspecified database');
 
-			conn = mdb.connect(host, user, passwd, Connection.__database)
-			Connection.__connection = conn
+			conn = mdb.connect(host, user, passwd, cls.__database)
+			cls.__connection = conn
 
 			# Set data retrieval return type as dictionary
-			Connection.__session = conn.cursor(mdb.cursors.DictCursor)
+			cls.__session = conn.cursor(mdb.cursors.DictCursor)
 
-			Connection.__session.execute("SELECT VERSION()");
-			ver = Connection.__session.fetchone()
+			cls.__session.execute("SELECT VERSION()");
+			ver = cls.__session.fetchone()
 			print "[SERVER] Database ver: %s " % ver
 
 		except mdb.Error as e:
 			print "Error %d: %s" % (e.args[0], e.args[1])
-			sys.exit(1)
+			exit(1)
 
-	## End def __connect
 
-	def close(self):	
-		if Connection.__session:
-			Connection.__session.close()
-			Connection.__connection.close()
-
-	## End def close
+	@classmethod
+	def close(cls):	
+		if cls.__session:
+			cls.__session.close()
+			cls.__connection.close()
 
 def main():
 	try:
-		tx = Connection()
+		tx = Database()
 		print "[Server] Test connection success"
 		tx.close()
 	except Exception, e:
 		print e
 		sys.exit(1)
+
 
 if __name__ == '__main__':
 	main()
