604a0ebdd1cce24f97b969191dffdea7df5cdef9
Omar Mousa <Omar@Omar-2.local>
2016-04-24 15:21:52 -0700

    admin class update with connect inported

--- server-side/Admin.py
+++ server-side/Admin.py
@@ -14,16 +14,25 @@ from Validater import *
 
 class Admin(Student):
 
-	def _init_(self):
-
-		super(Admin, self)._init_()
+		def __init__(self):
+		# super(Student, self).__init__()
+		# Get connection & cursor from Database
+		self.conn = super(Student, self).connect()
+		self.session = super(Student, self).getSession()
 	#function for an Admin to add a student into the database
 	def addStudent(self,studentID,studentEmail,FirstName,LastName,MiddleName=None):
 		self.session.execute("""INSERT INTO Student (SJSUID, Email, FirstName,'MiddleName', 'LastName'))
 		VALUES(SJSUID, Email, FirstName,MiddleName,LastName""")
-
+	#function for an Admin to remove a student from an organization
 	def quitOrganization(self, studentID, organizationName):
 		self.session.execute("""DELETE from Student (SJSUID,Email,FirstName,'MiddleName', 'LastName'))
 		VALUES(SJSUID, Email, FirstName,MiddleNme,LastName""")
+	#function for an Admin to check if a student is an active member of the organization
+	def _isStudentActiveMember(self, uidStudent, uidOrganization):
+		self.session.execute("""
+			SELECT * FROM MemberOf as mem
+				WHERE mem, 'Student_fk' = %s AND mem.'Organization_fk' = %s
+				AND mem.'Active' = '1'; 
+				""", (uidStudent, uidOrganization))					 )
 
 	
