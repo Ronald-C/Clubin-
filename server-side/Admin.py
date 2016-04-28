c768555a7c4a732c7024a4d7f9a839b28a800a7e
Omar Mousa <Omar@Omar-2.local>
2016-04-27 19:18:30 -0700

    admin class update with deletion and addition

--- server-side/Admin.py
+++ server-side/Admin.py
@@ -1,28 +1,26 @@
-from Officer import Officer
-from Student import Student
+from Officer import Officer #inheriting Officer Class
+#from Student import Student #inherting Student Class
 #from Validater import Validate
 
-class Admin(Officer):
+class Admin(Officer): # Admin class declaration
 
-	def _init_(self):
-			
+	def _init_(self): # initialzie object "self" this allows to call inherited functions from Student and Officer class			
 
-			super(Admin, self)._init_()
+			super(Admin, self)._init_() #
 
 	def addStudent(self, SJSUID, Email, FirstName, MiddleName, LastName):
-
-		#uidStudent = super(Admin, self)._getStudentUID(SJSUID)
-
 		
 		self.session.execute(""" 
 			INSERT INTO Student (SJSUID, Email, FirstName,MiddleName, LastName) 
 				VALUES (%s,%s,%s,%s,%s)""", (SJSUID,Email,FirstName, MiddleName,LastName))
 		self.conn.commit()
 		
-	#def removeStudent(self, FirstName, MiddleName,LastName):
+	def deleteStudent(self, SJSUID):
 
-	#	self.session.execute("""
-	##		DELETE from Student where FirstName='FirstName', MiddleName='MiddleName', LastName='LastName'
-	#		VALUES (%s,%s,%s)""",(FirstName,MiddleName,LastName))
-	#	self.conn.commit()
+		self.session.execute("""
+			DELETE from Student where SJSUID='%s'; """ % SJSUID)
+		self.conn.commit()
 
+	#def promoteStudent(self, SJSUID):
+
+	#	self.session.execute(""")
