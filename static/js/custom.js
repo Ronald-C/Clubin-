4714b3171512ed9a51198083487f15dbca72d80b
Justin Phan <justinvanphan@gmail.com>
2016-05-05 17:37:02 -0700

    After a successful sign up, redirect to login page.

--- static/js/custom.js
+++ static/js/custom.js
@@ -26,6 +26,53 @@ $(".orgWell").on("click", function() {
 $(".submitSingle").on("click", function() {
 
     //Front end error checking with toast messaging.
+
+        //Check for empty fields
+        var someEmpty = $('.mandatory').filter(function(){
+            return $.trim(this.value).length === 0;
+        }).length > 0;
+
+
+        if( someEmpty ) {
+            toastr.clear();
+            toastr["warning"]("Please give values for required inputs.");
+            return;
+        }
+
+
+        var digits = $("#SJSUIDSingle").val();
+
+        //Exactly 9 characters for SJSUID
+        if( digits.length != 9 ) {
+            toastr["warning"]("Please enter exactly 9 digits for your SJSUID.");
+            return;
+        }
+
+
+        //Numbers only fo SJSUID
+        if( !$.isNumeric(digits) ) {
+            toastr["warning"]("Numbers only for SJSUID");
+            return;
+            // your code here
+        }
+
+        //Passwords don't match
+        var p1 = $("#PasswordSingle").val();
+        var p2 = $("#PasswordSingleAgain").val();
+        if( p1 != p2 ) {
+            toastr["warning"]("Your passwords don't match.");
+            return;
+        }
+
+        //Password doesn't have all StormPath requirements
+        // var storm = new RegExp("^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/");
+        // if(storm.test(p1)) {
+        //     alert("Pass");
+
+        // }
+
+        var storm = /((?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,15})/gm;
+
     
 
 
@@ -36,13 +83,33 @@ $(".submitSingle").on("click", function() {
         data: $('#singleForm').serialize(),
         type: 'POST',
         success: function(response) {
-            console.log(response)
+            var jinjaObject = $.parseJSON(response);
+            console.log("Direct response is before parse: " + response);
+            console.log("Am I successful?: " + jinjaObject["SUCCESS"] );
+            console.log("Object after parsing is... ");
+            console.log(jinjaObject);
+
+            //If not successful
+            if(jinjaObject["SUCCESS"] != "1") {
+                $(".submitSingle").prop("disabled", false).text("Send").addClass("btn-primary").removeClass("btn-success");
+
+                //Loop through error object, toast the words.
+                var errObj = $.parseJSON( jinjaObject["ERROR"] );
+                for(var key in errObj) {
+                    toastr["warning"](errObj[key], key);
+                }
+
+            } else { //We were successful
+                console.log("time to move");
+                window.location.href = "/login";
+            }
         }
     })
 }); 
 
 $("#singleModal").on("hidden.bs.modal", function() {
     $(this).find(".submitSingle").prop("disabled", false).text("Send").addClass("btn-primary").removeClass("btn-success");
+    $(this).find("input").val("");
 });
 
 $("#formLogin").on("submit", function(event) {
