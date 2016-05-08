fe529bd58d3b370c4b4fe1629ff3aca1e3bb003d
Omar Mousa <Omar@Omar-2.local>
2016-05-08 14:07:17 -0700

    updated Admin class

--- server-side/Admin.py
+++ server-side/Admin.py
@@ -1,45 +1,57 @@
+#!/bin/python
+
 from Officer import Officer #inheriting Officer Class
 
-
-class Admin(Officer): # Admin class declaration
-
-	def _init_(self): # initialzie object "self" this allows to call inherited functions from Student and Officer class			
-
-			super(Admin, self)._init_() #super allows us to call functions from the Parent's Parent's class 
+# Admin class declaration
+class Admin(Officer): 
+	# initialzie object "self" this allows 
+	# to call inherited functions from Student and Officer class			
+	def _init_(self): 
+			super(Admin, self)._init_() 
 
 
 	def deActivateStudent(self,uidStudent):
-
+ 	# changes the active status of the student based on thier UID
+ 	# searches the student that needs to get deactivated
 		self.session.execute("""UPDATE MemberOf
-					SET MemberOf.`Active` = '0' # changes the active status of the student based on thier UID
-					WHERE MemberOf.`Student_fk` = %s  # searches the student that needs to get deactivated
+					SET MemberOf.`Active` = '0'
+					WHERE MemberOf.`Student_fk` = %s  
 						""" % (uidStudent))
 		self.conn.commit()
 
-	def isTroubleMaker(self,uidStudent, uidOfficer, uidOrganization): # function that inserts into the black list for an organization
+		# function that inserts into the black list for an organization
+	def isTroubleMaker(self,uidStudent, uidOfficer, uidOrganization): 
 		self.session.execute("""INSERT INTO TroubleMaker(Student_fk,Officer_fk,Organization_fk)
 					VALUES(%s,%s,%s)""" , (uidStudent,uidOfficer,uidOrganization))
 		self.conn.commit()
-	
-	def orgInfo(self,orgID,orgName,Descrip,Building,RoomNumber): # updating an organization info
+	 # updating an organization info
+	 # updates Name,Description,Building, RoomNumber based on the orgID number
+	def orgInfo(self,orgID,orgName,Descrip,Building,RoomNumber):
 		self.session.execute("""UPDATE Organization 
 					SET Organization.`OrganizationName` = %s, Organization.`Description` = %s,
 					Organization.`Building` = %s ,
 					Organization.`RoomNumber` = %s  
 					WHERE Organization.`OrganizationID` = %s
-					""", (orgID,orgName,Descrip,Building,RoomNumber)) # updates Name,Description,Building, RoomNumber based on the orgID number
+					""", (orgID,orgName,Descrip,Building,RoomNumber)) 
 		self.conn.commit()
 
-	def isAdmin(self,AdminR):
+
+	# check if student with specific foreign key 
+	# is an Officer of AdminRank
+	def isAdmin(self,AdminR): 
 
 		self.session.execute("""SELECT AdminRank
 								From OfficerOf
 								Where Student_fk = %s """ % AdminR)
 		self.conn.commit()
-		AdminR = self.session.fetchone() # note : fetchone() grabs the next row of a query
+		 # note : fetchone() grabs the next row of a query
+		AdminR = self.session.fetchone()
+		# if student is not of AdminRank {President, Vice PRes}
 		if not AdminR:
-			raise TypeError("Student is not an Admin")
+			raise TypeError("Student is not an Admin") 
 
 		else:
 			AdminR = AdminR['AdminRank']
 			return str(AdminR)
+
+	def isMemberOf(self,)
